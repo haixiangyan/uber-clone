d806a592632f8481810c0b184ea8cbcd6669e8e4
haixiangyan <haixiang6123@gmail.com>
2025-04-17 23:19:40 +0800

    feat: 完成返回按钮

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,5 +1,7 @@
+import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { View, Text } from 'react-native';
+import { Icon } from '@rneui/base';
+import { View, TouchableOpacity } from 'react-native';
 
 import Map from '../components/Map';
 import NavigateCard from '../components/NavigateCard';
@@ -8,8 +10,16 @@ import RideOptionsCard from '../components/RideOptionsCard';
 const MapScreen = () => {
   const Stack = createStackNavigator();
 
+  const navigation = useNavigation();
+
   return (
     <View>
+      <TouchableOpacity
+        className="absolute left-8 top-16 z-40 rounded-full bg-gray-100 p-3 shadow-lg"
+        onPress={() => navigation.goBack()}>
+        <Icon name="arrowleft" type="antdesign" />
+      </TouchableOpacity>
+
       <View className="h-1/2">
         <Map />
       </View>
